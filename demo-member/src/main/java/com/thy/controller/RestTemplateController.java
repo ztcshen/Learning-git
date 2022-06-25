bbea6fef167b7b5cdbeb82e956d6264ac8862b07
zhuolinhe <zhuolinhe@neta.onaliyun.com>
2022-06-25 13:42:58 +0800

    test

--- demo-member/src/main/java/com/thy/controller/RestTemplateController.java
+++ demo-member/src/main/java/com/thy/controller/RestTemplateController.java
@@ -18,6 +18,9 @@ public class RestTemplateController {
     @GetMapping(value = "rest-get", produces = "text/html;charset=utf-8")
     public String restTemplateGet() {
         //post请求是的方法是postForObject,第二个参数是返回值类型
+        // test git
+        // git
+        // w awe
         return restTemplate.getForObject("http://localhost:8090/order/queryUser?userId=12",
                 String.class);
 
